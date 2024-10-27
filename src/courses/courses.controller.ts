429bb4219d2ad67b7b9b9aa539257c06e2ed7dbf
Romario Ribeiro <romaryjane@hotmail.com>
2024-10-27 12:14:39 -0300

    Crud criado  no Service

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -1,7 +1,13 @@
 import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, Patch, Post, Res } from '@nestjs/common';
+import { CoursesService } from './courses.service';
 
 @Controller('courses')
 export class CoursesController {
+
+    constructor(
+        private readonly courseService: CoursesService
+    ) {}
+
 
     @Get()
     findAll(@Res() response) {
