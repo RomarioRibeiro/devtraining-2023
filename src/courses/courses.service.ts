4f92be485e928a06290997f63e7c6ac5fbd2ff9b
Romario Ribeiro <romaryjane@hotmail.com>
2024-11-11 07:45:18 -0300

    Criando  relations de entidade

--- src/courses/courses.service.ts
+++ src/courses/courses.service.ts
@@ -19,12 +19,15 @@ export class CoursesService {
 
 
     async findAll() {
-        return this.courseRepository.find()
+        return this.courseRepository.find( {
+            relations: ['tags']
+        } )
     }
 
     async findOne(id: number) {
         const course =  await this.courseRepository.findOne({
-            where: { id }
+            where: { id },
+            relations: ['tags']
         })
         if(!course) {
             throw new HttpException(`Course com Id ${id} not found`, HttpStatus.NOT_FOUND)
